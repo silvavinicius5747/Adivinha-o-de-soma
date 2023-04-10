e301b9af21bdf9d265a9c96fdc15f470ef6a64df
Vinicius <46089337+silvavinicius5747@users.noreply.github.com>
2023-04-10 14:29:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,25 @@
-// Seleciona os elementos HTML necessários
-const numbers = document.querySelectorAll('.number');
-const scoreElement = document.getElementById('score');
-const messageElement = document.querySelector('.message');
+var contador = 0;
 
-// Inicializa as variáveis necessárias
-let score = 0;
-let selectedNumbers = [];
-
-// Função que verifica se a soma dos números selecionados é igual a 10
-function checkSum() {
-  const sum = selectedNumbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-  return sum === 10;
+function gerarPergunta() {
+	var num1 = Math.floor(Math.random() * 9) + 1;
+	var num2 = Math.floor(Math.random() * 9) + 1;
+	document.getElementById("pergunta").innerHTML = num1 + " + " + num2 + " = ";
+	document.getElementById("resposta").value = "";
 }
 
-// Função que atualiza a mensagem final de acordo com o número de acertos
-function updateMessage() {
-  if (score === 3) {
-    messageElement.textContent = 'Parabéns! Você acertou 3 vezes!';
-  } else {
-    messageElement.textContent = `Acertos: ${score}`;
-  }
+function verificarResposta() {
+	var num1 = parseInt(document.getElementById("pergunta").innerHTML.split("+")[0]);
+	var num2 = parseInt(document.getElementById("pergunta").innerHTML.split("+")[1]);
+	var resposta = parseInt(document.getElementById("resposta").value);
+	if (resposta === num1 + num2) {
+		alert("Parabéns, você acertou!");
+		contador++;
+	} else {
+		alert("Que pena, você errou. A resposta correta é " + (num1 + num2));
+	}
+	document.getElementById("contador").innerHTML = "Acertos: " + contador;
+	gerarPergunta();
 }
 
-// Adiciona um listener de click para cada número
-numbers.forEach((number) => {
-  number.addEventListener('click', () => {
-    // Verifica se o número já foi selecionado
-    if (selectedNumbers.includes(Number(number.dataset.value))) {
-      // Remove o número da lista de selecionados
-      selectedNumbers = selectedNumbers.filter((selectedNumber) => selectedNumber !== Number(number.dataset.value));
-      // Remove a classe "selected" do número
-      number.classList.remove('selected');
-    } else {
-      // Adiciona o número à lista de selecionados
-      selectedNumbers.push(Number(number.dataset.value));
-      // Adiciona a classe "selected" ao número
-      number.classList.add('selected');
-    }
-
-    // Verifica se a soma dos números selecionados é igual a 10
-    if (checkSum()) {
-      // Incrementa o score e atualiza a mensagem final
-      score++;
-      updateMessage();
-      // Reseta a lista de números selecionados e remove a classe "selected" de todos os números
-      selectedNumbers = [];
-      numbers.forEach((number) => number.classList.remove('selected'));
-    }
-  });
-});
+gerarPergunta();
+document.getElementById("verificar").addEventListener("click", verificarResposta);
